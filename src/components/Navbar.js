096fa81fc1cb37337fb0d76d6a434a9f7a25d5ae
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-21 00:04:27 +0530

    navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,37 @@
 import React from "react";
 import IOSSwitch from "../utils/Switch";
-
+import "../css/Navbar.css";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import { BsSearch } from "react-icons/bs";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>Image Gallery</div>
-      <div>
-        <input type="text" />
+    <div className="navbarMain">
+      <div className="image cursor">Image Gallery</div>
+      <div className="inputField">
+        <div className="BsSearch">
+          <BsSearch />
+        </div>
+        <input type="text" placeholder="Search image here" />
       </div>
       <div>
-        <div>
-          <div>Explore</div>
-          <div>Collection</div>
-          <div>Community</div>
+        <div className="flex navBTN">
+          <div className="cursor">Explore</div>
+          <div className="collection cursor">Collection</div>
+          <div className="cursor">Community</div>
         </div>
       </div>
-      <div>
-        Dark Mode
+      <div className="flex">
+        <div className="mode">Dark Mode</div>
+        <div className="btn">
         <FormGroup>
           <FormControlLabel control={<IOSSwitch sx={{ m: 1 }} />} />
         </FormGroup>
+        </div>
       </div>
+      <div className="RxHamburgerMenu"><RxHamburgerMenu/></div>
     </div>
   );
 };
