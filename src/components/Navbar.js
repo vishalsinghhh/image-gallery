71505c6cf1bc02918306389ad5fef8f2a4f0111e
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-20 16:34:26 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import IOSSwitch from "../utils/Switch";
+
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <div>Image Gallery</div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <div>
+          <div>Explore</div>
+          <div>Collection</div>
+          <div>Community</div>
+        </div>
+      </div>
+      <div>
+        Dark Mode
+        <FormGroup>
+          <FormControlLabel control={<IOSSwitch sx={{ m: 1 }} />} />
+        </FormGroup>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
