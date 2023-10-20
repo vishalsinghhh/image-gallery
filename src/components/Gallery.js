5a1b1aacae46ebc19b12357f56d2e5cd69572ec1
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-21 05:06:43 +0530

    modal

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import "../css/Gallery.css";
 import like from "../static/like.svg";
 import like1 from "../static/like1.svg";
+import Modal from "./Modal";
 
 const Gallery = (props) => {
-  // console.log(props?.data[0]?.user?.name);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="gallery">
+    <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h2>Modal Content</h2>
+        <p>This is the content of the modal.</p>
+      </Modal>
       {props?.data?.map((item, i) => {
         return (
           <div key={i} className="pics">
             {item.urls.regular && (
-              <div>
+              <div onClick={()=>{openModal()}}>
                 <img src={item.urls.regular} alt="" style={{ width: "90%" }} />
                 <div className={!props?.light ? "details":"details detailsDark"}>
                   <div>
