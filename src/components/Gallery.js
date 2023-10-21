9245225f5de148a57fa4d3571d0dcb49b75e2578
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-21 05:35:01 +0530

    modal fix

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,12 +1,12 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "../css/Gallery.css";
 import like from "../static/like.svg";
 import like1 from "../static/like1.svg";
 import Modal from "./Modal";
-
+import cross from "../static/cross.svg";
 const Gallery = (props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [user, setUser] = useState()
+  const [user, setUser] = useState();
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -17,27 +17,54 @@ const Gallery = (props) => {
   };
   return (
     <div className="gallery">
-    <Modal isOpen={isModalOpen} onClose={closeModal}>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
         <div className="modalMain">
-          <div><img src={user?.urls.regular} alt="" style={{ width: "70%" }}/></div>
+          <div className="modalDetails">
+            <img src={user?.urls.regular} alt="" style={{ width: "70%" }} />
+            {/* <div> */}
+              <button
+                onClick={() => {
+                  setIsModalOpen(false);
+                }}
+                className="onClose"
+              >
+                <img src={cross} alt="" />
+              </button>
+            {/* </div> */}
+          </div>
         </div>
       </Modal>
       {props?.data?.map((item, i) => {
         return (
           <div key={i} className="pics">
             {item.urls.regular && (
-              <div onClick={()=>{openModal(); setUser(item)}}>
+              <div
+                onClick={() => {
+                  openModal();
+                  setUser(item);
+                }}
+              >
                 <img src={item.urls.regular} alt="" style={{ width: "90%" }} />
-                <div className={!props?.light ? "details":"details detailsDark"}>
+                <div
+                  className={!props?.light ? "details" : "details detailsDark"}
+                >
                   <div>
                     <img src={item.user.profile_image?.medium} alt="" />
                   </div>
                   <div>
-                    <div className={!props.light?"userName":'userName userNameDark'}>{item.user?.name}</div>
-                    <div className="insta">@{item.user?.instagram_username}</div>
+                    <div
+                      className={
+                        !props.light ? "userName" : "userName userNameDark"
+                      }
+                    >
+                      {item.user?.name}
+                    </div>
+                    <div className="insta">
+                      @{item.user?.instagram_username}
+                    </div>
                   </div>
                   <div className="like">
-                    <img src={!props.light?like1:like} alt="" />
+                    <img src={!props.light ? like1 : like} alt="" />
                     <div>{item.likes}</div>
                   </div>
                 </div>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "../css/Modal.css";
-import cross from "../static/cross.svg";
 
-const Modal = ({ isOpen, onClose, children }) => {
+
+const Modal = ({ isOpen, children }) => {
   if (!isOpen) return null;
 
   return (
     <div className="modal">
       <div className="modal-content">
-        <button onClick={onClose} className="onClose">
-          <img src={cross} alt="" />
-        </button>
+        
         {children}
       </div>
     </div>
