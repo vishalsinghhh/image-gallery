8cd8dd005c7b870e39845b05e61d1db2f7101b38
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-21 14:01:08 +0530

    new changes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,6 +4,8 @@ import like from "../static/like.svg";
 import like1 from "../static/like1.svg";
 import Modal from "./Modal";
 import cross from "../static/cross.svg";
+import insta from "../static/instagram.svg";
+import twitter from "../static/twitter.svg";
 const Gallery = (props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [user, setUser] = useState();
@@ -15,12 +17,18 @@ const Gallery = (props) => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+  console.log(user);
   return (
     <div className="gallery">
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <div className="modalMain">
           <div className="modalDetails">
-            <img src={user?.urls.regular} alt="" style={{ width: "70%" }} />
+            <img
+              src={user?.urls.regular}
+              alt=""
+              style={{ width: "70%" }}
+              className="modalImage"
+            />
             <div>
               <button
                 onClick={() => {
@@ -31,7 +39,20 @@ const Gallery = (props) => {
                 <img src={cross} alt="" />
               </button>
             </div>
-            <div className="detailsModal"></div>
+            <div className="detailsModal">
+              <div className="detailsModalMain">
+                <div>
+                  <img src={user?.user.profile_image?.medium} alt="" />
+                </div>
+                <div>{user?.user.name}</div>
+                <div>
+                  <img src={insta} alt="" /> / {user?.user.instagram_username}
+                </div>
+                <div>
+                  <img src={twitter} alt="" /> / {user?.user.twitter_username}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </Modal>
@@ -60,9 +81,7 @@ const Gallery = (props) => {
                     >
                       {item.user?.name}
                     </div>
-                    <div className="insta">
-                      @{item.user?.instagram_username}
-                    </div>
+                    <div className="insta">@{item.user?.username}</div>
                   </div>
                   <div className="like">
                     <img src={!props.light ? like1 : like} alt="" />
